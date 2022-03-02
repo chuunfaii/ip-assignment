2390ddf92bf25c6beefb9242eaf8c6e9cf6f5145
CallMeJaX <87804946+C411M3J4X@users.noreply.github.com>
2022-03-03 00:34:20 +0800

    Fix Scrollbar when open modal

--- include/footer.php
+++ include/footer.php
@@ -31,6 +31,21 @@
         $(document).ready(function () {
             $('#dataTable').DataTable();
         });
+        //Prevent Content Move while Modal Pop Up
+        $(document).ready(function () {
+            $('.modal').on('show.bs.modal', function () {
+                if ($(document).height() > $(window).height()) {
+                    // no-scroll
+                    $('body').addClass("modal-open-noscroll");
+                }
+                else {
+                    $('body').removeClass("modal-open-noscroll");
+                }
+            });
+            $('.modal').on('hide.bs.modal', function () {
+                $('body').removeClass("modal-open-noscroll");
+            });
+        });
     </script>
 
   </body>
