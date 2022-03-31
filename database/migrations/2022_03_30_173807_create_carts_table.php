b082768c0fd6050d0ef39343562d9930bdd17e5a
CallMeJaX <87804946+C411M3J4X@users.noreply.github.com>
2022-03-31 16:36:57 +0800

    modify cart table

--- database/migrations/2022_03_30_173807_create_carts_table.php
+++ database/migrations/2022_03_30_173807_create_carts_table.php
@@ -14,8 +14,7 @@ class CreateCartsTable extends Migration
     public function up()
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->id();
-            $table->string('customer_id');
+            $table->string('user_id');
             $table->string('artwork_id');
             $table->string('quantity');
             $table->timestamps();
