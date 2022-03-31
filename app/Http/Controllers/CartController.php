8da201d93f1803dca7ef503a183211cdae6dd84a
CallMeJaX <87804946+C411M3J4X@users.noreply.github.com>
2022-03-31 17:24:47 +0800

    setup cart file

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -36,7 +36,7 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/cart.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/wishlist.css') }}" />
 @endsection
 
 @section('content')
-    <main class="h-100 mx-auto my-5 d-flex">
-        @if($cart != null)
-            @foreach($cart as $c)
-            <div class="row my-5" id="1">
-                <span id="lblArtworkId" style="display: none;">1</span>
+<div style="flex-grow: 1;">
+        <div class="container m-auto h-100 position-relative">
+            <div class="d-flex justify-content-center align-items-center">
+                <h1 class="navbar-brand fw-bold p-0 php m-0 mt-5">Your Cart</h1>
+            </div>
+            @if($cart != null)
+                @foreach($cart as $c)
+                <div class="row my-5" id="3">
+                    <span id="lblArtworkId" style="display: none;">1</span>
                     <div class="col-2">
                         <div class="d-flex justify-content-center">
                             <a href="./">
-                                <img src="" alt="" class="card-img-top" style="max-height: 8rem;">
+                                <img src="https://i.pinimg.com/564x/26/cf/3c/26cf3c80b7b5923f89fba8fe140dd660.jpg" alt="" class="card-img-top" style="max-height: 8rem;">
                             </a>
                         </div>
                     </div>
@@ -21,7 +25,7 @@
                         <div class="h-100 d-flex flex-column justify-content-between">
                             <span>
                                 <a href="./" class="text-decoration-none text-muted fs-4">
-                                    ee
+                                    Construction in Red, Blue & Yellow Drawing
                                 </a>
                             </span>
                             <hr>
@@ -36,21 +40,23 @@
                     <div class="col-2">
                         <div class="h-100 d-flex align-items-center justify-content-between">
                             <button id="btnRemove" class="btn btn-outline-danger">Remove</button>
-                            <button id="btnCart" class="btn btn-primary">Add to Cart</button>
                         </div>
                     </div>
                 </div>
-            @endforeach
-        @else
-            <div class="d-flex flex-column justify-content-between align-items-center">
-                <div class="d-flex justify-content-center align-items-center">
-                    <h1 class="display-4">Your Cart</h1>
+                @endforeach
+            @else
+                <div class="d-flex flex-column justify-content-between align-items-center">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h1 class="display-4">Your Cart</h1>
+                    </div>
+                    <div class="d-flex flex-column h-100 justify-content-center align-items-center mb-5">
+                        <h1 class="display-6 mb-5">Your cart is empty right now.</h1>
+                        <a href="{{ url('artworks') }}" class="btn btn-primary mt-3 py-3 px-4">Browse Artwork</a>
+                    </div>
                 </div>
-                <div class="d-flex flex-column h-100 justify-content-center align-items-center mb-5">
-                    <h1 class="display-6 mb-5">Your cart is empty right now.</h1>
-                    <a href="{{ url('artworks') }}" class="btn btn-primary mt-3 py-3 px-4">Browse Artwork</a>
-                </div>
-            </div>
-        @endif
-    </main>
+            @endif
+
+        </div>
+    </div>
+
 @endsection
