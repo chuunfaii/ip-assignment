4b8a454a5833e3a558e809f301ddb2469caabb28
Chun Fai <ashchunfai@gmail.com>
2022-03-31 15:52:22 +0800

    Fix artwork images url in ArtworkSeeder

--- database/seeders/ArtworkSeeder.php
+++ database/seeders/ArtworkSeeder.php
@@ -16,9 +16,12 @@ class ArtworkSeeder extends Seeder
     public function run()
     {
         $faker = Factory::create();
+        $current_num = 0;
 
         // Paintings
         for ($i = 1; $i <= 7; $i++) {
+            $current_num = $i;
+
             Artwork::create([
                 'name' => 'Painting ' . $i,
                 'quantity' => rand(10, 50),
@@ -32,12 +35,14 @@ class ArtworkSeeder extends Seeder
 
         // Photography
         for ($i = 1; $i <= 7; $i++) {
+            $current_num++;
+
             Artwork::create([
                 'name' => 'Photography ' . $i,
                 'quantity' => rand(10, 50),
                 'price' => rand(15000, 150000),
                 'description' => $faker->text(),
-                'image_url' => 'artwork-' . $i . '.jpg',
+                'image_url' => 'artwork-' . $current_num . '.jpg',
                 'user_id' => rand(6, 10),
                 'category_id' => 2,
             ]);
@@ -45,12 +50,14 @@ class ArtworkSeeder extends Seeder
 
         // Drawings
         for ($i = 1; $i <= 7; $i++) {
+            $current_num++;
+
             Artwork::create([
                 'name' => 'Drawing ' . $i,
                 'quantity' => rand(10, 50),
                 'price' => rand(15000, 150000),
                 'description' => $faker->text(),
-                'image_url' => 'artwork-' . $i . '.jpg',
+                'image_url' => 'artwork-' . $current_num . '.jpg',
                 'user_id' => rand(6, 10),
                 'category_id' => 3,
             ]);
@@ -58,12 +65,14 @@ class ArtworkSeeder extends Seeder
 
         // Sculpture
         for ($i = 1; $i <= 7; $i++) {
+            $current_num++;
+
             Artwork::create([
                 'name' => 'Sculpture ' . $i,
                 'quantity' => rand(10, 50),
                 'price' => rand(15000, 150000),
                 'description' => $faker->text(),
-                'image_url' => 'artwork-' . $i . '.jpg',
+                'image_url' => 'artwork-' . $current_num . '.jpg',
                 'user_id' => rand(6, 10),
                 'category_id' => 4,
             ]);
