1fbc1eb19f433e1c3213727ac428793ee3d45a51
Chun Fai <ashchunfai@gmail.com>
2022-04-02 04:42:32 +0800

    Refactor code for artwork details page

--- resources/views/pages/artwork-details.blade.php
+++ resources/views/pages/artwork-details.blade.php
@@ -2,16 +2,15 @@
 
 @section('css')
 
-<link rel="stylesheet" href="{{ asset('css/artwork-detail.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/artwork-details.css') }}" />
 
 @endsection
 
 @section('content')
 
-<form class="m-auto" action="{{ route('wishlist_and_cart', $artwork->id) }}" method="POST">
+<form class="m-auto" action="{{ route('wishlist-cart', $artwork->id) }}" method="POST">
     @csrf
-    <div class="container d-flex" style="min-height: 500px;">
-    
+    <div class="container d-flex" style="min-height: 500px;"> 
         <div class="d-flex justify-content-center" style="flex: 1;">
             <img style="max-width: 70%;" src="{{ asset('upload/artworks/' . $artwork->image_url) }}" />
         </div>
@@ -25,7 +24,6 @@
                         {{ $artwork->description }}
                     </p>
                 </div>
-
                 <div class="d-flex justify-content-between mb-5">
                     <span class="text-muted">
                         {{ $artwork->category->name }}
@@ -34,34 +32,33 @@
                         {{ $artwork->artist->presentFullName() }}
                     </span>
                 </div>
-
                 <div>
                     <p class="fw-bold">
-                        {{ $artwork->quantity }} Quantity Available
+                        {{ $artwork->quantity }} stock(s) available
                     </p>
-
                     <div class="d-flex justify-content-between align-items-end">
                         <div>
                             <span class="me-3">Quantity:</span>
-                            <input id='txtQuantity' name="quantity" type='number' min='1' max='{{ $artwork->quantity }}' value='1' />
+                            <input name="quantity" type='number' min='1' max='{{ $artwork->quantity }}' value='1' />
+                            @error('quantity')
+                                <p class="m-0 mt-3 error-message">{{ $message }}</p>
+                            @enderror
                         </div>
-
-                        <span class="display-6">{{ $artwork->presentPrice()}} </span>
+                        <div class="my-auto display-6">{{ $artwork->presentPrice() }}</div>
                     </div>
                 </div>
             </div>
-
             <div class="d-flex justify-content-between align-items-end h-25">
                 <a class="fs-5 text-muted text-decoration-none" href="{{ route('artworks') }}">
                     <i class="bi bi-caret-left-fill"></i> Back to Artworks
                 </a>
-
                 <div>
-                    <button id="btnWishlist" class="btn btn-outline-secondary px-4 py-3 me-3 " name="action" value="wishlist">
+                    <button type="submit" class="btn btn-outline-secondary px-4 py-3 me-3" name="action" value="wishlist">
                         <i class="bi bi-heart-fill"></i>
                     </button>
-                    <button id="btnCart" class="btn btn-primary px-5 py-3" name="action" value="cart">Add to
-                        Cart</button>
+                    <button type="submit" class="btn btn-primary px-5 py-3" name="action" value="cart">
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         </div>
