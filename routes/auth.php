b637c1a031457cefb82b72235fea28dd7b3c41b0
Chun Fai <ashchunfai@gmail.com>
2022-04-04 12:37:50 +0800

    Refactor code for routes

--- routes/auth.php
+++ routes/auth.php
@@ -11,73 +11,86 @@ use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\OrderHistoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-  // Register
-
-  Route::get('/register', [RegisterController::class, 'create'])
+Route::middleware('guest')->group(function() {
+    // Register
+    Route::get('/register', [RegisterController::class, 'create'])
             ->name('register');
 
-  Route::post('/register', [RegisterController::class, 'store']);
+    Route::post('/register', [RegisterController::class, 'store']);
 
-  // Login
-
-  Route::get('/login', [LoginController::class, 'create'])
+    // Login
+    Route::get('/login', [LoginController::class, 'create'])
             ->name('login');
 
-  Route::post('/login', [LoginController::class, 'store']);
+    Route::post('/login', [LoginController::class, 'store']);
 });
 
-Route::middleware('auth')->group(function () {
-  // Customer & Artist - Edit Account
-
-  Route::get('/account/edit', [UserController::class, 'create'])
+Route::middleware('auth')->group(function() {
+    // Customer & Artist - Edit Account
+    Route::get('/account/edit', [UserController::class, 'create'])
             ->name('edit-account');
 
-  Route::post('/account/edit', [UserController::class, 'store']);
+    Route::post('/account/edit', [UserController::class, 'store']);
 
-  Route::post('/account/delete', [UserController::class, 'destroy'])
+    Route::post('/account/delete', [UserController::class, 'destroy'])
             ->name('delete-account');
 
-  // Artist - My Artworks
-
-  Route::get('/my-artworks', [ArtistArtworkController::class, 'index'])
+    // Artist - My Artworks
+    Route::get('/my-artworks', [ArtistArtworkController::class, 'index'])
             ->name('my-artworks')
             ->middleware('role:artist');
 
-  // Customer - Cart & Wishlist-Cart
+    Route::post('/store-artwork', [ArtistArtworkController::class, 'store'])
+            ->middleware('role:artist');
 
-  Route::get('/cart', [CartController::class, 'index'])
-            ->name('cart');
-  
-  Route::post('/update-cart', [CartController::class, 'updateCart']);
-  
-  Route::post('/wishlist-cart/{id}', [ArtworkController::class, 'wishlist_cart'])
+    Route::post('/fetch-artwork', [ArtistArtworkController::class, 'edit'])
+            ->middleware('role:artist');
+
+    Route::post('/update-artwork', [ArtistArtworkController::class, 'update'])
+            ->middleware('role:artist');
+
+    Route::post('/delete-artwork', [ArtistArtworkController::class, 'destroy'])
+            ->middleware('role:artist');
+
+    // Artist - My Sales
+    Route::get('/my-sales',[MySalesController::class, 'index'])
+            ->name('my-sales')
+            ->middleware('role:artist');
+
+    // Customer - Cart & Wishlist-Cart
+    Route::get('/cart', [CartController::class, 'index'])
+            ->name('cart')
+            ->middleware('role:customer');
+
+    Route::post('/update-cart', [CartController::class, 'updateCart']);
+
+    Route::post('/wishlist-cart/{id}', [ArtworkController::class, 'wishlist_cart'])
             ->name('wishlist-cart')
             ->middleware('role:customer');
-  
-  // Customer - Wishlist
 
-  Route::get('/wishlist', [WishlistController::class, 'index'])
-            ->name('wishlist');
-  
-  Route::post('/update-wishlist',[WishlistController::class, 'updateWishlist']);
-  
-  // Customer - Checkout
+    // Customer - Wishlist
+    Route::get('/wishlist', [WishlistController::class, 'index'])
+            ->name('wishlist')
+            ->middleware('role:customer');
 
-  Route::post('/checkout', [OrderController::class, 'checkout'])
+    Route::post('/update-wishlist',[WishlistController::class, 'update'])
+            ->middleware('role:customer');
+
+    // Customer - Checkout
+    Route::post('/checkout', [OrderController::class, 'checkout'])
             ->name('checkout')
             ->middleware('role:customer');
-            
-  // Customer - Thanks
+                
+    // Customer - Thanks
+    Route::get('/thanks', [OrderController::class, 'index'])
+            ->name('thanks')
+            ->middleware('role:customer');
 
-  Route::get('/thanks', [OrderController::class, 'index'])
-            ->name('thanks');
-
-  Route::get('/order-history',[OrderHistoryController::class,'index'])
-            ->name('order-history');
-            
-  // Customer & Artist - Logout
-
-  Route::post('/logout', [LoginController::class, 'destroy'])
+    Route::get('/order-history',[OrderHistoryController::class, 'index'])
+            ->name('order-history')
+            ->middleware('role:customer');
+                
+    // Customer & Artist - Logout
+    Route::post('/logout', [LoginController::class, 'destroy'])
             ->name('logout');
 });
--- routes/web.php
+++ routes/web.php
@@ -23,32 +23,17 @@ use App\Models\Artwork;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])
+        ->name('home');
 
-//Artist Add, Update, Delete & Retrieve Artworks
-Route::post('/store-artwork', [ArtistArtworkController::class, 'store']);
-Route::post('/fetch-artwork', [ArtistArtworkController::class, 'edit']);
-Route::post('/update-artwork', [ArtistArtworkController::class, 'update']);
-Route::post('/delete-artwork', [ArtistArtworkController::class, 'destroy']);
-
-Route::get('/my-sales', function () {
-    return view('pages.my-sales');
-})->name('my-sales');
-
-// Route::get('/order-history', function () {
-//     return view('pages.order-history');
-// });
-
-Route::get('/artists', [ArtistController::class, 'index'])->name('artists');
+Route::get('/artists', [ArtistController::class, 'index'])
+        ->name('artists');
 
 Route::get('/artist/{id}', [ArtistController::class, 'show']);
 
-Route::get('/artworks', [ArtworkController::class, 'index'])->name('artworks');
+Route::get('/artworks', [ArtworkController::class, 'index'])
+        ->name('artworks');
 
 Route::get('/artwork/{id}', [ArtworkController::class, 'show']);
-
-
-
-Route::get('/my-sales',[MySalesController::class,'index'])->name('my-sales');
 
 require __DIR__ . '/auth.php';
