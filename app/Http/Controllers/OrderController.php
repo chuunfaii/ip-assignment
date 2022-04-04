d65ff285a5f414e242f4a7ab9cf95b2be0a9ddfe
Chun Fai <ashchunfai@gmail.com>
2022-04-04 13:38:09 +0800

    Refactor code for Order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -22,12 +22,12 @@ class OrderController extends Controller
     {
         $now = Carbon::now()->toDateTimeString();
 
-        $customer_id = auth()->user()->id;
+        $user_id = auth()->user()->id;
 
-        $carts = Cart::all()->where('user_id', $customer_id);
+        $carts = Cart::all()->where('user_id', $user_id);
 
         $order_id = DB::table('orders')->insertGetId([
-            'user_id' => $customer_id,
+            'user_id' => $user_id,
             'created_at' => $now,
             'updated_at' => $now,
         ]);
@@ -48,75 +48,9 @@ class OrderController extends Controller
             $artwork->save();
         }
 
-        Cart::where('user_id', $customer_id)->delete();
+        Cart::where('user_id', $user_id)->delete();
 
         return view('pages.thanks');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     public function checkout()
@@ -125,9 +59,11 @@ class OrderController extends Controller
 
         $stripe = new StripeClient('sk_test_51KkOjTJC2EY2ixMBbB9tD5XbwSDydaBRzB7i6gAvlOdbWbfT1dT3KgLYj6LFp6xwq7MsUw6XyPaPHChjdV3tBmqg00NopvLdXY');
 
+        $user_id = auth()->user()->id;
+
         $line_items = [];
 
-        $carts = Cart::all()->where('user_id', auth()->user()->id);
+        $carts = Cart::all()->where('user_id', $user_id);
 
         foreach ($carts as $cart) {
             $line_item = [
