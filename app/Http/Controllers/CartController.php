03964aefa82584e0423da06e8e62e5853a42110f
Chun Fai <ashchunfai@gmail.com>
2022-04-04 13:32:39 +0800

    Refactor code for Cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,7 +15,10 @@ class CartController extends Controller
      */
     public function index()
     {
-        $carts = Cart::all()->where('user_id', auth()->user()->id);
+        $user_id = auth()->user()->id;
+
+        $carts = Cart::all()->where('user_id', $user_id);
+
         $total_price = 0;
 
         foreach ($carts as $cart) {
@@ -26,85 +29,32 @@ class CartController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function updateCart(Request $request)
+    public function update(Request $request)
     {
-        switch ($request->input('cartBtn')) {
+        switch ($request->input('action')) {
             case 'update':
-                $id = $request->input('actionId');
-                $cart = Cart::find($id);
-                $cart->quantity = $request->input('quantity');
-                $cart->save();
-                return redirect()->back()->with('message', 'Quantity Updated Successfully');
+                $cart_id = $request->input('cart_id');
 
-                break;
+                $cart = Cart::find($cart_id);
+
+                $cart->quantity = $request->input('quantity');
+
+                $cart->save();
+
+                return redirect()->back()->with('message', 'Quantity has been updated successfully.');
 
             case 'remove':
-                $id = $request->input('actionId');
-                $cart = Cart::find($id);
-                $cart->delete();
-                return redirect()->back()->with('message', 'Cart Removed Successfully');
+                $cart_id = $request->input('cart_id');
 
-                break;
+                Cart::find($cart_id)->delete();
+
+                return redirect()->back()->with('message', 'Cart item has been removed successfully.');
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Request $request)
-    {
-
     }
 }
